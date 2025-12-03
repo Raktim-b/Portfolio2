59876fe4b6b764e5ca82a006059f0f1b22a19c28
Raktim-b <raktimb414@gmail.com>
2025-12-03 16:19:35 +0530

    autoscroll added

--- script/script.js
+++ script/script.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggler-btn");
   const navToggler = document.getElementById("nav-collapsed");
   const body = document.body;
+  const html = document.documentElement;
 
   toggleBtn.addEventListener("click", () => {
     navToggler.classList.toggle("translate-x-0");
+    
+    html.classList.toggle("overflow-hidden");
     body.classList.toggle("overflow-hidden");
   });
 });
